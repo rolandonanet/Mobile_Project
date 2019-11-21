e6b53f01b4ef3b40cf0ac435ca55272a2e0eadb5
Gabriel <gabrielroland1998@gmail.com>
2019-11-21 01:20:45 -0300

    QRCODE

--- app/src/main/java/com/project/presence/activity/TeacherActivity.java
+++ app/src/main/java/com/project/presence/activity/TeacherActivity.java
@@ -3,9 +3,16 @@ package com.project.presence.activity;
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentTransaction;
 
 import com.ittianyu.bottomnavigationviewex.BottomNavigationViewEx;
 import com.project.presence.R;
+import com.project.presence.fragment.ClassTeacherFragment;
+import com.project.presence.fragment.HomeStudentFragment;
+import com.project.presence.fragment.HomeTeacherFragment;
+import com.project.presence.fragment.MissTeacherFragment;
+import com.project.presence.fragment.QRCodeTeacherFragment;
 
 import android.content.Intent;
 import android.os.Bundle;
@@ -15,7 +22,7 @@ import android.view.MenuItem;
 import android.widget.Switch;
 
 public class TeacherActivity extends AppCompatActivity {
-
+    private BottomNavigationViewEx navigationView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -24,10 +31,43 @@ public class TeacherActivity extends AppCompatActivity {
         Toolbar toolbar = findViewById(R.id.toolbarPrincipal);
         toolbar.setTitle("Nome do Professor");
         setSupportActionBar(toolbar);
+        navigationView = findViewById(R.id.bottom_navigation_teacher);
+        bottomNavigation(navigationView);
+        FragmentManager fragmentManager = getSupportFragmentManager();
+        FragmentTransaction fragmentTransaction = fragmentManager.beginTransaction();
+        fragmentTransaction.replace(R.id.viewPagerTeacher, new HomeTeacherFragment()).commit();
+
     }
 
-    private void configureBottomNavigationView(){
-        BottomNavigationViewEx bottomNavigationViewEx = findViewById(R.id.bottom_navigation_teacher);
+    public void bottomNavigation(BottomNavigationViewEx viewEx){
+        viewEx.setOnNavigationItemSelectedListener(new BottomNavigationViewEx.OnNavigationItemSelectedListener() {
+            @Override
+            public boolean onNavigationItemSelected(@NonNull MenuItem menuItem) {
+
+                FragmentManager fragmentManager = getSupportFragmentManager();
+                FragmentTransaction fragmentTransaction = fragmentManager.beginTransaction();
+
+                switch (menuItem.getItemId()){
+                    case R.id.ic_home_teacher :
+                        fragmentTransaction.replace(R.id.viewPagerTeacher, new HomeTeacherFragment()).commit();
+                        return true;
+
+                    case R.id.ic_miss_teacher :
+                        fragmentTransaction.replace(R.id.viewPagerTeacher, new MissTeacherFragment()).commit();
+                        return true;
+
+                    case R.id.ic_class_teacher :
+                        fragmentTransaction.replace(R.id.viewPagerTeacher, new ClassTeacherFragment()).commit();
+                        return true;
+
+                    case R.id.ic_qrcode_teacher :
+                        fragmentTransaction.replace(R.id.viewPagerTeacher, new QRCodeTeacherFragment()).commit();
+                        return true;
+                }
+
+                return false;
+            }
+        });
     }
 
     @Override
---
+++ app/src/main/java/com/project/presence/fragment/MissTeacherFragment.java
@@ -0,0 +1,32 @@
+package com.project.presence.fragment;
+
+
+import android.os.Bundle;
+
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.project.presence.R;
+
+/**
+ * A simple {@link Fragment} subclass.
+ */
+public class MissTeacherFragment extends Fragment {
+
+
+    public MissTeacherFragment() {
+        // Required empty public constructor
+    }
+
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_miss_teacher, container, false);
+    }
+
+}
