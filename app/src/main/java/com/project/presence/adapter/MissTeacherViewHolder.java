537ebb5bea81f3bc54484b8f91d938beb41b128e
Rafael <rafael.ssanches95@gmail.com>
2019-11-25 18:11:42 -0300

    Home e miss Fragment

---
+++ app/src/main/java/com/project/presence/adapter/MissTeacherViewHolder.java
@@ -0,0 +1,25 @@
+package com.project.presence.adapter;
+
+import android.view.View;
+import android.widget.TextView;
+
+import androidx.recyclerview.widget.RecyclerView;
+
+import com.project.presence.R;
+
+public class HomeTeacherViewHolder extends RecyclerView.ViewHolder {
+    public TextView teacher_timeTV;
+    public TextView teacher_subjectTV;
+    public TextView teacher_clasroomTV;
+    public TextView teacher_studentAmmountTV;
+    public TextView teacher_themeTV;
+
+    public HomeTeacherViewHolder (View itemView) {
+        super (itemView);
+        this.teacher_timeTV = itemView.findViewById(R.id.teacher_timeTextView);
+        this.teacher_subjectTV = itemView.findViewById(R.id.teacher_subjectTextView);
+        this.teacher_clasroomTV = itemView.findViewById(R.id.teacher_classroomTextView);
+        this.teacher_studentAmmountTV = itemView.findViewById(R.id.teacher_studentAmmountTextView);
+        this.teacher_themeTV = itemView.findViewById(R.id.teacher_themeTextView);
+    }
+}
